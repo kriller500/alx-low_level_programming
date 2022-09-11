091850b88c573266f946dc2785bdc2e50eda93a8
kriller500 <jumadaniel275@students.uonbi.ac.ke>
2022-09-11 08:34:00 -0700

    Adding 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -6,7 +8,7 @@
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	write(STDOUT_FILENO, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
 
 	return (1);
 }
