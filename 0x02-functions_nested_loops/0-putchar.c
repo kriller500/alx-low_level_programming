d346b29f62ca31c05b941334ffb0d6beb9fae9fa
kriller500 <jumadaniel275@students.uonbi.ac.ke>
2022-09-13 08:43:42 -0700

    Adding 0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() 
+{
+	/*Write C code here*/
+	 int ch;
+	putchar(ch);
+	ch ='_';
+	putchar(ch);
+	ch = 'p';
+	putchar(ch);
+	ch = 'u';
+	putchar(ch);
+	ch = 't';
+	putchar(ch);
+	ch = 'c';
+	putchar(ch);
+	ch = 'h';
+	putchar(ch);
+	ch = 'a';
+	putchar(ch);
+	ch = 'r';
+	putchar(ch);
+	printf("\n");
+	return 0;
+}
